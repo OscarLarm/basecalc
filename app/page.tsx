b4804f6ca6d8b26c7338df8aa3fd1574fc1f8314
Oscar <118494885+oscar-larm@users.noreply.github.com>
2025-11-28 02:09:23 +0100

    Update layout of site. Include BaseCardContainer in page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
 import Image from "next/image";
+import InputCard from "./components/InputCard";
+import NavigationBar from "./components/NavigationBar";
+import BaseCardContainer from "./components/BaseCardContainer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-neutral-950 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-neutral-950">
-        <h1 className="text-7xl font-mono">0xffffe</h1>
-      </main>
-    </div>
+    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-neutral-950">
+      <div className="flex-1 flex-col flex items-center justify-start gap-16">
+        <InputCard />
+        <BaseCardContainer />
+
+      </div>
+    </main>
   );
 }
