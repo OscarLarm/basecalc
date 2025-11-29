5bc60f71304584fe15bef0bea36f66c80a10e8d3
OscarLarm <118494885+OscarLarm@users.noreply.github.com>
2025-11-29 18:59:39 +0100

    Add less padding to mobile viewports.

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
 
 
   return (
-    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center pt-32 px-16 bg-neutral-950">
+    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center pt-8 sm:pt-32 px-4 sm:px-16 bg-neutral-950 transition-all duration-200">
       <div className="flex-1 flex-col w-full flex items-stretch justify-start gap-4">
         <InputCard title="decimal" prefix="" placeholder="0" value={decimalDisplay} base="10" onChange={handleDecimalChange} />
         <InputCard title="hexadecimal" prefix="0x" placeholder="0" value={hexDisplay} base="16" onChange={handleHexChange} />
